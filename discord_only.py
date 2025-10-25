f8dafcb749b2a6afcd27ed4ee099b2549ec38fd1
GitSW7 <solaris7@runbox.com>
2025-10-24 19:26:02 -0500

    Refactor message handling and logging format

--- discord_only.py
+++ discord_only.py
@@ -7,22 +7,21 @@ load_dotenv()
 
 # Set up intents
 intents = discord.Intents.default()
-intents.message_content = True # Ensure that your bot can read message content
+intents.message_content = True  # Ensure that your bot can read message content
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print("We have logged in as {0.user}'.format(clien
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-if message.content.startswith('$hello'):
-     await message.channel.send('Hello!)
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 client.run(os.getenv('TOKEN'))
-                                
-    
+
